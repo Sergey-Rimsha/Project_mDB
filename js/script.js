12151cbad7560217ce66716645b9f66cdb8ee0b0
Sergey <Sergey.rimsha.ru@gmail.com>
2020-09-20 12:43:48 +0300

    practice_desktop

--- js/script.js
+++ js/script.js
@@ -1,92 +1,91 @@
-/* Задания на урок:
-
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-
-5) Добавить нумерацию выведенных фильмов */
-
 'use strict';
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ],
-    hidePromo: function() {
-        const promo = document.querySelector('.promo__adv');
-        const promoImage = promo.querySelectorAll('img');
-        
-        promoImage.forEach(item => {
-            item.remove();
+
+document.addEventListener('DOMContentLoaded', () => {
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ],
+        hidePromo: function () {
+            const promo = document.querySelector('.promo__adv');
+            const promoImage = promo.querySelectorAll('img');
+
+            promoImage.forEach(item => {
+                item.remove();
+            });
+
+        },
+        changeGenre: function () {
+            const genre = document.querySelector('.promo__genre');
+
+            genre.textContent = 'драма';
+        },
+        changeImage: function () {
+            const newImg = document.querySelector('.promo__bg');
+
+            newImg.style.backgroundImage = 'url(./img/bg.jpg)';
+        },
+        changeFilms: function () {
+            const films = document.querySelector('.promo__interactive-list');
+
+            this.movies.sort();
+            films.innerHTML = '';
+
+            this.movies.forEach((item, i) => {
+                films.innerHTML += `
+                    <li class="promo__interactive-item">${i + 1} ${this.movies[i]}
+                        <div class="delete"></div>
+                    </li>`;
+            });
+
+        },
+        pushFilms: function () {
+
+            const inputValue = document.querySelector('.adding__input').value;
+            if (inputValue.length > 21) {
+                let strNew = inputValue.slice(0, 21);
+                movieDB.movies.push(strNew + '...');
+            } else {
+                movieDB.movies.push(inputValue);
+            }
+        }
+    };
+
+    const btn = document.querySelector('.add button');
+    btn.addEventListener('click', function (e) {
+        e.preventDefault();
+        movieDB.pushFilms();
+        movieDB.changeFilms();
+
+
+    });
+
+    const delFilm = document.querySelectorAll('.delete');
+    //console.log(delFilm);
+    delFilm.forEach((btn, i) => {
+        console.log(btn.parentElement);
+        btn.addEventListener('click', () => {
+            btn.parentElement.remove();
+            movieDB.movies.splice(i, 1);
+            console.log(btn);
+            movieDB.pushFilms();
+            movieDB.changeFilms();
         });
+    });
 
-    },
-    changeGenre: function() {
-        const genre = document.querySelector('.promo__genre');
-
-        genre.textContent = 'драма';
-    },
-    changeImage: function() {
-        const newImg = document.querySelector('.promo__bg');
-
-        newImg.style.backgroundImage = 'url(./img/bg.jpg)';
-    },
-    changeFilms: function() {
-        const films = document.querySelectorAll('.promo__interactive-item');
-
-        this.movies.sort();
-
-        films.forEach((item, i) => {
-            item.textContent = `${i + 1} ${this.movies[i]}`;
-        });
-
-    },
-    pushFilms: function() {
-       
-        const inputValue = document.querySelector('.adding__input').value;  
-        console.log(inputValue);
-        movieDB.movies.push(inputValue);
-                
-    }
-};
-
-const btn = document.querySelector('.add button');
-btn.addEventListener('click', function(e) {
-    e.preventDefault();
-        
+    movieDB.hidePromo();
+    movieDB.changeGenre();
+    movieDB.changeImage();
     movieDB.changeFilms();
-    movieDB.pushFilms();
-    
+    //movieDB.pushFilms();
+
+
+
+
+
+
 });
-
-movieDB.hidePromo();
-movieDB.changeGenre();
-movieDB.changeImage();
-movieDB.changeFilms();
-//movieDB.pushFilms();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
